90837e0f98194d84c1e2c6298f587cef1de6253f
George <letsokomolefe02@gmail.com>
2022-03-07 13:59:58 +0200

    added necessary logic for the 2nd number to be stored in a variable once operation variable is active

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ let deleter = $('#delete');
 let equal = $('#equal');
 let operates = $$('.operation')
 
+//returns + - * / or error depending on the operator
 const operate = (operator, a, b) => {
 	if(operator === multiple) {
 		return multiply(a, b);
@@ -40,25 +41,46 @@ const operate = (operator, a, b) => {
 }
 
 let number1;
+let number2;
 let operation;
 
+//if the operation variable is not active return number1 variable; 
 numbers.forEach(number => {
 	number.addEventListener('click', e => { 
-		number1 = display.textContent += e.target.id;
-		return number1;
+		if(!operation) {
+			number1 = display.textContent += e.target.id;
+			return number1;
+		}
 	});
 });
 
 operates.forEach(operat => {
 	operat.addEventListener('click', e => {
 		display.textContent += operat.textContent;
-		operation = $('#' + e.srcElement.id);
+		operation = $('#' + e.srcElement.id); //had to use this since e.target.id does not return the desired result;
 		return number1 + operation;
 	});
 });
 
-number2 = 2;
+//if the operation variable is active return number2 variable;
+numbers.forEach(number => {
+	number.addEventListener('click', e => { 
+		if(operation) {
+			number2 = display1.textContent += e.target.id;
+			return number2;
+		}
+	});
+});
+
+clear.addEventListener('click', e => {
+	display.textContent = '';
+	display1.textContent = '';
+	number1 = '';
+	number2 = '';
+	operation = undefined;
+})
 
 equal.addEventListener('click', e => { 
+	display1.textContent = '';
 	display.textContent = operate(operation, Number(number1), Number(number2));
 });
