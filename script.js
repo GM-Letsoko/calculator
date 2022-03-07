4e2194f78d5d748b749b9cd6a68ed7536fec631d
George <letsokomolefe02@gmail.com>
2022-03-07 12:57:23 +0200

    added style.css, index.html with boilerplate and basic math functions to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const divider = function(a, b) {
+	return a / b;
+};
+
+const multiple = function(a, b) {
+	return a * b;
+};
+
