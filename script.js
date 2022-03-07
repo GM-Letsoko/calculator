842450f5b73ed89603535a7aec2f858d1a7c5237
George <letsokomolefe02@gmail.com>
2022-03-07 13:09:02 +0200

    added operate function to script.js

--- script.js
+++ script.js
@@ -13,7 +13,29 @@ const divider = function(a, b) {
 	return a / b;
 };
 
-const multiple = function(a, b) {
+const multiply = function(a, b) {
 	return a * b;
 };
 
+let display = $('#display');
+let display1 = $('#display1');
+let numbers = $$('.numbers');
+let clear = $('#clear');
+let deleter = $('#delete');
+let equal = $('#equal');
+
+const operate = (operator, a, b) => {
+	if(operator === multiple) {
+		return multiply(a, b);
+	} else if(operator === minus) {
+		return subtract(a, b);
+	} else if(operator === plus) {
+		return add(a, b);
+	} else if(operator === divide) {
+		return divider(a, b);
+	} else {
+		return 'ERROR';
+	}
+}
+
+console.log(operate(multiple, 2, 3));
