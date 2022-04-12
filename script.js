e29fb4cdd2e1990b9e5d22f7c35e26b8ab3b7946
George <letsokomolefe02@gmail.com>
2022-04-12 16:28:28 +0200

    changed logic so that number1 and number2 share the same display

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+'use strict';
+
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+
+const display = $('#display');
+let display2 = $('#display2');
+let numbers = $$('.numbers');
+let clear = $('#clear');
+let deleter = $('#delete');
+let equal = $('#equal');
+let operates = $$('.operation')
+let home = $('#home');
+
+//returns + - * / or error depending on the operator
+const operate = (operator, a, b) => {
+	if(operator === '*') {
+		return multiplier([a, b]);
+	} else if(operator === '-') {
+		return subtractor([a, b]);
+	} else if(operator === '+') {
+		return sum([a, b]);
+	} else if(operator === '/') {
+		return divider([a, b]);
+	} 
+}
+
+const subtractor = function(array) {
+	let subtract = (a, b) => a - b;
+	return array.reduce(subtract);
+};
 
 const sum = function(array) {
 	let add = (a, b) => a + b;
@@ -16,34 +45,6 @@ const divider = function(array) {
 	return array.reduce(divider);
 };
 
-const subtractor = function(array) {
-	let subtract = (a, b) => a - b;
-	return array.reduce(subtract);
-};
-
-let display = $('#display');
-let display1 = $('#display1');
-let display2 = $('#display2');
-let numbers = $$('.numbers');
-let clear = $('#clear');
-let deleter = $('#delete');
-let equal = $('#equal');
-let operates = $$('.operation')
-let home = $('#home');
-
-//returns + - * / or error depending on the operator
-const operate = (operator, a, b) => {
-	if(operator === multiple) {
-		return multiplier([a, b]);
-	} else if(operator === minus) {
-		return subtractor([a, b]);
-	} else if(operator === plus) {
-		return sum([a, b]);
-	} else if(operator === divide) {
-		return divider([a, b]);
-	} 
-}
-
 let number1;
 let number2;
 let operation;
@@ -51,6 +52,7 @@ let operation;
 //if the operation variable is not active return number1 variable; 
 numbers.forEach(number => {
 	number.addEventListener('click', e => { 
+		if(e.target.id === '.' && display.textContent.includes('.')) return;
 		if(!operation) {
 			number1 = display.textContent += e.target.id;
 			return number1;
@@ -60,25 +62,33 @@ numbers.forEach(number => {
 
 operates.forEach(operat => {
 	operat.addEventListener('click', e => {
+		if(number2) { 
+			number1 = bottom_display.textContent;
+			operation = undefined;
+		} 
+		if(e.target.classList.value === 'operation' && display.textContent.includes(operation)) return;
 		display.textContent += operat.textContent;
-		operation = $('#' + e.srcElement.id); //had to use this since e.target.id does not return the desired result;
-		return number1 + operation;
+		operation = e.target.id;
+		number2 = '';
+		bottom_display.textContent = '';
 	});
 });
 
 //if the operation variable is active return number2 variable;
 numbers.forEach(number => {
 	number.addEventListener('click', e => { 
+		if(e.target.id === '.' && display1.textContent.includes('.')) return;
 		if(operation) {
-			number2 = display1.textContent += e.target.id;
-			number2;
-		} display2.textContent = operate(operation, Number(number1), Number(number2));
+			display.textContent += e.target.id; 
+			number2 += e.target.id;
+			number2; 
+			display2.textContent = operate(operation, Number(number1), Number(number2));
+		} 
 	});
 });
 
 clear.addEventListener('click', e => {
 	display.textContent = '';
-	display1.textContent = '';
 	display2.textContent = '';
 	number1 = '';
 	number2 = '';
@@ -86,19 +96,14 @@ clear.addEventListener('click', e => {
 });
 
 deleter.addEventListener('click', e => {
-	if(display1.textContent) { 
-		display1.textContent = display1.textContent.substr(0, display1.textContent.length - 1);
-		number2 = display1.textContent;
-		//display2.textContent = display2.textContent.substr(0, display2.textContent.length - 1);
-		display2.textContent = operate(operation, Number(number1), Number(number2));
-	} else if(display.textContent) { 
+	if(display.textContent) { 
 		display.textContent = display.textContent.substr(0, display.textContent.length - 1);
+		display2.textContent = display2.textContent.substr(0, display2.textContent.length - 1);;
 	} 
 });
 
 equal.addEventListener('click', e => { 
-	display1.textContent = '';
 	display2.textContent = '';
-	display.textContent = operate(operation, Number(number1), Number(number2)).toFixed(3);
+	display.textContent = operate(operation, Number(number1), Number(number2));
 	number1 = display.textContent;
 });
